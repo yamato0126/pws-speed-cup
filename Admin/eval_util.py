85e3b3acdf2f41c091bbbc1bdfa59995d1f4bacc
yamato0126 <craft1038jp@gmail.com>
2023-12-12 22:33:25 +0900

    refactor: 有用性評価と安全性評価の一部処理をcommon.pyに共通化

--- Admin/eval_util.py
+++ Admin/eval_util.py
@@ -1,31 +1,15 @@
 import pandas as pd
 
-def compare_csv_files(file_path1, file_path2):
-    # Read the CSV files
-    df1 = pd.read_csv(file_path1, header=None)
-    df2 = pd.read_csv(file_path2, header=None)
-
-    # Ensure the dimensions match
-    if df1.shape != df2.shape:
-        raise ValueError("CSV files have different dimensions")
-
-    # Count the number of differing elements
-    differing_elements_count = (df1 != df2).sum().sum()
-
-    # Calculate the total number of elements
-    total_elements = df1.shape[0] * df1.shape[1]
-
-    # Calculate the differing rate
-    differing_rate = (differing_elements_count / total_elements)
-
-    return differing_rate
+from common import calculate_differing_rate
 
 # File paths
 file_path1 = '../Docker/src/o.csv'
 file_path2 = '../Docker/src/a.csv'
+df1 = pd.read_csv(file_path1, header=None)
+df2 = pd.read_csv(file_path2, header=None)
 
 # Compute the differing rate
-differing_rate = compare_csv_files(file_path1, file_path2)
+differing_rate = calculate_differing_rate(df1, df2)
 print(f"utility:{1 - differing_rate}")
 with open('../Docker/src/util.txt', 'w') as f:
     print(1 - differing_rate, file=f)
--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ## ファイルの配置
 ```
 ├── Admin
-│   ├── attack_common.py
-│   ├── attack_type1.py
+│   ├── attack_1.py
+│   ├── common.py
 │   ├── eval_util.py
 │   └── generator.py
 └── Docker
@@ -18,7 +18,7 @@
 2. 各参加者が提出したソースコードを実行して加工データa.csvを得る．同時に実行時間を計測し，time.txtに出力する．
 3. eval_util.pyを実行してa.csvの有用性を評価し，util.txtに出力する．
 4. 加工データa.csvをソートして整列加工データb.csvを得る．
-5. o.csvの公開部とb.csvからo.csvの秘密部を推定する攻撃によって安全性を評価し，safe.txtに出力する．なお，攻撃プログラムはattack_type1.pyなど数種類存在する．
+5. o.csvの公開部とb.csvからo.csvの秘密部を推定する攻撃によって安全性を評価し，safe.txtに出力する．なお，攻撃プログラムはattack_1.pyなど数種類存在する．
 
 ## パラメータについて
 - データの行数 n = 10\**5 or 10\**6
